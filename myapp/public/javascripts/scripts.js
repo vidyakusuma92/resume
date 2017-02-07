495286455e682fc029f525879d2b8b0af6f26995
Vidya Kusuma <vk013901@Broadcom.net>
2017-02-07 17:32:44 +0530

    resume

---
+++ myapp/public/javascripts/scripts.js
@@ -0,0 +1,13 @@
+$('.parallax-window').parallax({imageSrc: 'https://static.pexels.com/photos/5770/leaf-spring-leaves-plants.jpg'});
+$(function(){
+        $(".element").typed({
+            strings: ["Frontend Developer.", "Vidya Kusuma"],
+            typeSpeed: 50,
+            cursorChar: "|",
+            loop: false,
+            loopcount: false,
+            showCursor: true,
+            backDelay: 4000
+
+        });
+    });
