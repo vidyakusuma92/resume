93badf95a9745ba95d1d29f9cd16ff7b3a04c5bc
Vidya Kusuma <Vidya Kusuma>
2017-02-07 23:46:53 +0530

    progress

--- myapp/public/javascripts/scripts.js
+++ myapp/public/javascripts/scripts.js
@@ -1,4 +1,5 @@
 $('.parallax-window').parallax({imageSrc: 'https://static.pexels.com/photos/5770/leaf-spring-leaves-plants.jpg'});
+$('.parallax-window-second').parallax({imageSrc: 'https://static.pexels.com/photos/6088/wood-forest-fire-fireplace.jpg'});
 $(function(){
         $(".element").typed({
             strings: ["Frontend Developer.", "Vidya Kusuma"],
